372f0629be535da7bb78194ff44f699be519b932
SIGgodsil <139847247+SIGgodsil@users.noreply.github.com>
2023-07-18 13:46:18 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,113 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'Design T-Shirt',
+        image: 't-shirt.PNG',
+        price: 120000
+    },
+    {
+        id: 2,
+        name: 'Black Man Jacket',
+        image: 'jacket.PNG',
+        price: 500000
+    },
+    {
+        id: 3,
+        name: 'Grey Men Jacket',
+        image: 'jacket 2.PNG',
+        price: 500000
+    },
+    {
+        id: 4,
+        name: 'Nylon Jacket',
+        image: 'jacket 3.PNG',
+        price: 300000
+    },
+    {
+        id: 5,
+        name: 'Fur Women Jacket', 
+        image: 'jacket 6.PNG',
+        price: 450000
+    },
+    {
+        id: 6,
+        name: 'Sport Jacket',
+        image: 'jacket 5.PNG',
+        price: 400000
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');;
+        newDiv.classList.add('AddToCard');;
+        
+        newDiv.innerHTML = `
+            <img src="image/${value.image}" width="50px" height="250px"/>
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        
+        list.appendChild(newDiv);
+    })
+}
+
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+        
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
